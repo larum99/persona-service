db02f7efe62c1d3faf2b4f4ec668c4085b10236a
Laura <laura.barona@uao.edu.co>
2025-10-20 05:35:19 -0500

    Setup project

---
+++ src/main/java/com/onclass/persona/PersonaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.onclass.persona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onclass/persona/PersonaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onclass.persona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
